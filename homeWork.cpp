e7b70dcf5eacc2f072272bec918abd9226c2acbe
hasibprangon <hasibulhossainp09@gmail.com>
2022-12-07 22:38:00 +0600

    comment added

--- homeWork.cpp
+++ homeWork.cpp
@@ -8,13 +8,14 @@ int main()
     cout<<"Enter the number of Row : " ;
     cin>>n;
 
-    for(i=1; i<=n; i++)
+    for(i=1; i<=n; i++) //loop for creating row
     {
-        for(j=1; j<=n-i; j++)
+        for(j=1; j<=n-i; j++) //loop for creating left side space for piramid star pattern
         {
             cout<<" ";
         }
         for(k=1 ; k<=2*i-1 ; k++)
+         //Here I am printing star as piramid. condition k<=2*i-1 : 2 * number of row -1 ( Since every time two stars are increase)
         {
             cout<<"*";
         }
